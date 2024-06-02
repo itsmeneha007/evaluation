169840084ad945821e82df42c7e1e581c4250b82
itsmeneha007 <nehabaghel11092003@gmail.com>
2024-06-02 17:14:28 +0530

    first question

---
+++ question1.js
@@ -0,0 +1,10 @@
+
+
+function extractSubArray(array){
+    return array.slice(3, 7);
+}
+const animals = ["Dogs", "Cat", "Elephant", "Giraffe", "Lion", "Tiger", "Zebra", "Kangaroo", "Panda", "Monkey"]
+const result = extractSubarray(animals);
+
+
+console.log(result);
